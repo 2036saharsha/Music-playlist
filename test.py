95b07c403b1f40fb610cf3d8e1cad85651b87aff
Saharsha Tiwari <61298966+2036saharsha@users.noreply.github.com>
2022-11-17 10:24:35 -0500

    Delete test.py

--- test.py
+++
@@ -1,5 +0,0 @@
-string="In the Name of Love", "Martin Garrix, Bebe Rexha", "The Less I Know The Better", "Tame Impala", "Reptilia", "The Strokes", "My Type", "Saint Motel", "Sound & Color", "Alabama Shakes", "Preach", "Drake, PARTYNEXTDOOR", "Move", "Saint Motel", "The Palisades", "Childish Gambino, Christian Rich", "YOU'RE THE ONE (feat. Syd)", "KAYTRANADA, Syd"
-songsList=[]
-for val in string:
-    songsList.append(val)
-print(songsList)
